2dff6a86f807bd0711b7c9d25fc7fd4d57ac7267
XiN <s090821847@gmail.com>
2024-12-02 15:56:24 +0800

    v1.0

---
+++ ApiRoutes/users.js
@@ -0,0 +1,15 @@
+import express from "express"
+import { verifyAdmin, verifyUser } from "../JWT_Token.js"
+import { deletedUser, getAllUsers, getUser, updateUser } from "../RoutesController/user.js"
+
+const router = express.Router()
+//更新user
+router.put("/:id",updateUser)
+//刪除
+router.delete("/:id",deletedUser)
+//讀取 單一用戶資料
+router.get("/:id",getUser)
+//讀取全部用戶資料
+router.get("/",getAllUsers)
+
+export default router
