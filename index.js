853c8ae7815d700a881ed0b37581f50e09c23614
XiN <s090821847@gmail.com>
2024-12-04 15:26:55 +0800

    修正CORS的問題

--- index.js
+++ index.js
@@ -11,6 +11,22 @@ import orderApiRoute from "./ApiRoutes/order.js"
  
 import cookieParser from "cookie-parser"
 import cors from "cors"
+
+const allowedOrigins = ['https://boovago-frontend.onrender.com'];
+app.use(cors({
+    origin: function(origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
+  }));
+  
+
 const app = express()
 dotenv.config()
 
